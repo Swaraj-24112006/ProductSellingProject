e73346e9de6d00fd001926e45b944f2b6a6729f0
Swaraj-24112006 <swarajvmatre@gmail.com>
2025-12-18 23:18:09 +0530

    Update payment.controller.js

--- Payment/src/controller/payment.controller.js
+++ Payment/src/controller/payment.controller.js
@@ -1,5 +1,13 @@
 const paymentModel = require('../models/payment.model');
 const axios = require('axios');
+require('dotenv').config();
+const Razorpay = require('razorpay');
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
 
 async function createPayment(req, res) {
   const token = req.cookies.token || req.headers?.authorization?.split(' ')[1];
